9554dcb5338a834033770b3a987a7e30cd64db02
Luis Gustavo <86069439+0CyanLouis@users.noreply.github.com>
2024-08-29 23:03:06 +0000

    adicionando comentarios e uma estrutura basica

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
+//include de bibliotecas
 #include <stdio.h>
-//
+//definição de variaveis  
 char nome [55];
 char cpf [55];
 char email[55];
@@ -8,4 +9,9 @@ void create();
 void read();
 void update();
 void delete();
+//inicio do codigo
+int main (){
+printf ("rodando");
 
+//fim do codigo     
+}
